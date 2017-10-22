f1683c273372e5f52047a1cc6b39ee31a770e80f
Martin Karlsen <1martinkarlsen@gmail.com>
2017-10-22 17:32:24 +0200

    Changed replyTo queue

--- send.js
+++ send.js
@@ -5,10 +5,12 @@ module.exports = {
         amqp.connect('amqp://datdb.cphbusiness.dk', function(err, conn) {
             conn.createChannel(function(err, ch) {
                 var exchangeName = "Databasserne_BankRabbitMQ";
-                var replyToChannel = "Databasserne_NodeReply";
+                var replyToChannel = "Databasserne_Normalizer";
 
                 ch.assertExchange(exchangeName, 'fanout', { durable: false });
-                ch.publish(exchangeName, '', new Buffer(msg), { replyTo: replyToChannel });
+                ch.publish(exchangeName, '', new Buffer(msg), { 
+					replyTo: replyToChannel
+				});
             });
         });
     }
